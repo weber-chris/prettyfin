fcf505392fcc495e84a0e5651d132d495ffc1dff
Christoph Weber <christoph.t.weber@gmail.com>
2020-01-23 18:04:38 +0100

    implemented animation

--- frontend/app.py
+++ frontend/app.py
@@ -2,71 +2,91 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import time
-
+import plotly_express as px
+from pathlib import Path
+import os
+import json
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+
+folder_preprocessed_data = Path(os.getcwd()).parent / 'data' / 'preprocessed'
+df_ausgaben = pd.read_csv(folder_preprocessed_data / 'df_ausgaben_all.csv', index_col=0)
+df_einnahmen = pd.read_csv(folder_preprocessed_data / 'df_einnahmen_all.csv', index_col=0)
+
+with open(folder_preprocessed_data / 'funkt_id_map.json', 'r') as file:
+    funkt_id_map = json.load(file)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-slider'),
-    dcc.Interval(id='auto-stepper',
-                 interval=5 * 1000,  # in milliseconds
-                 n_intervals=0
-                 ),
-    dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
-        updatemode='drag',
-        step=None
-    ),
-    html.Div('bla', id='text-output'),
-    html.Button('Start', id='start-button'),
-    html.Button('Stop', id='stop-button'),
-    html.Div(id='hidden-div', style={'display': 'none'})
+    dcc.Graph(id='graph-with-slider', style={"width": "100%", "display": "inline-block"}),
+    # dcc.Dropdown(
+    #     id="continent-dropdown",
+    #     options=[{'label': 'Asia', 'value': 'Asia'}, {'label': 'Europe', 'value': 'Europe'},
+    #              {'label': 'Africa', 'value': 'Africa'},
+    #              {'label': 'Americas', 'value': 'Americas'}, {'label': 'Oceania', 'value': 'Oceania'}],
+    #     value='All', multi=True),
+    dcc.Dropdown(
+        id="x-axis-dropdown",
+        options=[{'label': cat[1], 'value': cat[0]} for cat in funkt_id_map.items()],
+        value='0', multi=False),
+    dcc.Dropdown(
+        id="y-axis-dropdown",
+        options=[{'label': cat[1], 'value': cat[0]} for cat in funkt_id_map.items()],
+        value='1', multi=False)
 ])
 
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    [Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(dict(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
+    [Input('x-axis-dropdown', 'value'), Input('y-axis-dropdown', 'value')])
+def update_figure(x_axis, y_axis):
+    print(x_axis)
+    print(y_axis)
+    fig = px.scatter(df_ausgaben, x=x_axis, y=y_axis, animation_frame=df_ausgaben.index, animation_group="canton",
+                     color="canton", hover_name="canton",
+                     log_x=True, size_max=55, range_x=[0.9*df_ausgaben[x_axis].min(), 1.1*df_ausgaben[x_axis].max()],
+                     range_y=[0.9*df_ausgaben[y_axis].min(), 1.1*df_ausgaben[y_axis].max()])
+    # fig.update_layout(showlegend=False)
+    return fig
 
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range': [2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition={'duration': 1000},
-        )
-    }
+
+# @app.callback(
+#     Output('graph-with-slider', 'figure'),
+#     [Input('year-slider', 'value')])
+# def update_figure(selected_year):
+#     filtered_df = df[df.year == selected_year]
+#     traces = []
+#     for i in filtered_df.continent.unique():
+#         df_by_continent = filtered_df[filtered_df['continent'] == i]
+#         traces.append(dict(
+#             x=df_by_continent['gdpPercap'],
+#             y=df_by_continent['lifeExp'],
+#             text=df_by_continent['country'],
+#             mode='markers',
+#             opacity=0.7,
+#             marker={
+#                 'size': 15,
+#                 'line': {'width': 0.5, 'color': 'white'}
+#             },
+#             name=i
+#         ))
+#
+#     return {
+#         'data': traces,
+#         'layout': dict(
+#             xaxis={'type': 'log', 'title': 'GDP Per Capita',
+#                    'range': [2.3, 4.8]},
+#             yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+#             margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+#             legend={'x': 0, 'y': 1},
+#             hovermode='closest',
+#             transition={'duration': 1000},
+#         )
+#     }
 
 
 # @app.callback(
