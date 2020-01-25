27f50c9062951c82a582c368a11879c73abb7266
Christoph Weber <christoph.t.weber@gmail.com>
2020-01-25 02:56:18 +0100

    v0.1 of gapminder style

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import plotly.express as px
+import plotly.graph_objects as go
 from pathlib import Path
 import os
 import json
@@ -14,63 +14,89 @@ import time
 folder_preprocessed_data = Path(os.getcwd()) / 'data' / 'preprocessed'
 df_ausgaben = pd.read_csv(folder_preprocessed_data / 'df_ausgaben_all_merged.csv', index_col=0)
 # df_einnahmen = pd.read_csv(folder_preprocessed_data / 'df_einnahmen_all.csv', index_col=0)
+
+with open(folder_preprocessed_data / 'funkt_id_map.json', 'r') as file:
+    funkt_id_map = json.load(file)
+with open(folder_preprocessed_data / 'iso_canton_map.json', 'r') as file:
+    iso_canton_map = json.load(file)
+with open(folder_preprocessed_data / 'population_id_map.json', 'r') as file:
+    population_id_map = json.load(file)
+
 min_year = df_ausgaben['year'].min()
 max_year = df_ausgaben['year'].max()
 year_ticks = {str(y): str(y) for y in df_ausgaben['year'].unique()}
 init_year = 1991
 cantons = df_ausgaben['canton'].unique()
-
-with open(folder_preprocessed_data / 'funkt_id_map.json', 'r') as file:
-    funkt_id_map = json.load(file)
+cantons.sort()  # needed so that legend is alphabetical
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    # dcc.Graph(id='graph-with-slider', style={"width": "100%", "display": "inline-block"}),
-    dcc.Graph(id='graph-bubbles', style={"width": "100%", "display": "inline-block"}),
-    dcc.Slider(
-        id='year-slider',
-        min=min_year,
-        max=max_year,
-        value=init_year,
-        marks=year_ticks,
-        updatemode='drag',
-        step=None
-    ),
-    dcc.Dropdown(
-        id="x-axis-dropdown",
-        options=[{'label': cat[1], 'value': cat[0]} for cat in funkt_id_map.items()],
-        value='0', multi=False, style={'width': '50%', 'display': 'inline-block'}),
-    dcc.Dropdown(
-        id="y-axis-dropdown",
-        options=[{'label': cat[1], 'value': cat[0]} for cat in funkt_id_map.items()],
-        value='1', multi=False, style={'width': '50%', 'display': 'inline-block'}),
-    html.Button(html.P('Start', id='start-button-text'), id='start-button'),
+    # dcc.Graph(id='graph-with-slider', style={'width': '100%', 'display': 'inline-block'}),
+    dcc.Graph(id='graph-bubbles', style={'width': '100%', 'height': '65vh'}),
+    html.Div([
 
-    dcc.Interval(id="interval", disabled=True, interval=1500),
-    html.P(id="output"),
-])
+        html.Div([html.Button(html.P('Play', id='start-button-text', style={'width': '5%', }), id='start-button')],
+                 style={'width': '6%'}),
+        html.Div([
+            dcc.Slider(
+                id='year-slider',
+                min=min_year,
+                max=max_year,
+                value=init_year,
+                marks=year_ticks,
+                updatemode='drag',
+                step=None
+            )], style={'width': '90%'})],
+        style={'display': 'flex', 'margin': '20px 0px 20px 0px', 'align-items': 'center'}),
+    html.Div([
+        html.Div([dcc.RadioItems(
+            id='normalize-radio',
+            options=[
+                {'label': 'Absolute', 'value': 'absolute'},
+                {'label': 'Normalized', 'value': 'normalized'}
+            ],
+            value='absolute'
+        )], style={'width': '6%'}),
+        html.Div([dcc.Dropdown(
+            id='x-axis-dropdown',
+            options=[{'label': cat[1], 'value': cat[0]} for cat in funkt_id_map.items()],
+            value='0', multi=False, style={'width': '30vw'})]),
+        html.Div([dcc.Dropdown(
+            id='y-axis-dropdown',
+            options=[{'label': cat[1], 'value': cat[0]} for cat in funkt_id_map.items()],
+            value='1', multi=False, style={'width': '30vw'})]
+        ),
+        html.Div([dcc.Dropdown(
+            id='size-dropdown',
+            options=[{'label': cat[1], 'value': cat[0]} for cat in population_id_map.items()],
+            value='population', multi=False, style={'width': '30vw'})]
+        )
+    ], style={'display': 'flex', 'align-items': 'center'}),
+
+    dcc.Interval(id='interval', disabled=True, interval=1500),
+], style={'width': '100%'})
 
 
-@app.callback(Output('start-button-text', 'children'), [Input("interval", "disabled")])
+@app.callback(Output('start-button-text', 'children'), [Input('interval', 'disabled')])
 def change_button_text(disabled):
     if disabled:
-        return 'Start'
+        return 'Play'
     else:
         return 'Stop'
 
 
-# @app.callback(Output("output", "children"), [Input("interval", "n_intervals")])
+# @app.callback(Output('output', 'children'), [Input('interval', 'n_intervals')])
 # def display_count(n):
-#     return f"Interval has fired {n} times"
+#     return f'Interval has fired {n} times'
 
 
-@app.callback([Output('year-slider', 'value'), Output("interval", "disabled")],
-              [Input("interval", "n_intervals"), Input("start-button", "n_clicks")],
+@app.callback([Output('year-slider', 'value'), Output('interval', 'disabled')],
+              [Input('interval', 'n_intervals'), Input('start-button', 'n_clicks')],
               [State('year-slider', 'value'),
-               State("interval", "disabled")])
+               State('interval', 'disabled')])
 def slide_adjust(n_intervals, n_clicks, slider_year, disabled):
     ctx = dash.callback_context
 
@@ -101,40 +127,66 @@ def slide_adjust(n_intervals, n_clicks, slider_year, disabled):
     Output('graph-bubbles', 'figure'),
     [Input('x-axis-dropdown', 'value'),
      Input('y-axis-dropdown', 'value'),
+     Input('size-dropdown', 'value'),
+     Input('normalize-radio', 'value'),
      Input('year-slider', 'value')]
 )
-def update_bubble(x_axis, y_axis, selected_year):
+def update_bubble(x_axis, y_axis, bubble_size_dropdown, normalize, selected_year):
     print('update_bubble')
 
     df_filtered = df_ausgaben[df_ausgaben.year == selected_year]
     traces = []
     for canton in cantons:
         df_canton = df_filtered[df_filtered['canton'] == canton]
+
+        if normalize == 'normalized':
+            x_vals = min_max_normalization(df_canton,df_ausgaben, x_axis, canton)
+            y_vals = min_max_normalization(df_canton,df_ausgaben, y_axis, canton)
+        else:
+            x_vals = df_canton[x_axis]
+            y_vals = df_canton[y_axis]
+
         traces.append(dict(
-            x=df_canton[x_axis],
-            y=df_canton[y_axis],
-            text=df_canton['canton'],
+            x=x_vals,
+            y=y_vals,
+            # x=df_canton[x_axis],
+            # y=df_canton[y_axis],
+            text=df_canton['canton'].apply(lambda x: iso_canton_map[x]),
             mode='markers',
             opacity=0.7,
             marker={
-                'size': df_canton['population'] * 0.0001,
+                'size': df_canton[bubble_size_dropdown] / df_ausgaben[bubble_size_dropdown].max() * 200,
                 'line': {'width': 0.5, 'color': 'white'}
             },
-            name=canton
+            name=iso_canton_map[canton]
         ))
 
+    if normalize == 'normalized':
+        x_range = [0, 1]
+        y_range = [0, 1]
+    else:
+        x_range = [df_ausgaben[x_axis].min(), df_ausgaben[x_axis].max()]
+        y_range = [df_ausgaben[y_axis].min(), df_ausgaben[y_axis].max()]
     fig = {
         'data': traces,
         'layout': dict(
-            xaxis={'title': funkt_id_map[x_axis], 'range': [df_ausgaben[x_axis].min(), df_ausgaben[x_axis].max()]},
-            yaxis={'title': funkt_id_map[y_axis], 'range': [df_ausgaben[y_axis].min(), df_ausgaben[y_axis].max()]},
-            margin={'l': 60, 'b': 40, 't': 10, 'r': 20},
-            legend={'x': 1, 'y': 1},
+            xaxis={'title': {'text': funkt_id_map[x_axis], 'font': {'size': 24}},
+                   'range': x_range},
+            yaxis={'title': {'text': funkt_id_map[y_axis], 'font': {'size': 24}},
+                   'range': y_range},
+            margin={'l': 100, 'b': 80, 't': 10, 'r': 20},
+            legend={'x': 1, 'y': 1, 'font': {'size': 13}},
             hovermode='closest',
             transition={'duration': 1000},
         )}
 
     return fig
+
+
+def min_max_normalization(df_canton_year,df_all, axis, canton):
+    df_canton_all = df_all[df_all['canton'] == canton][axis]
+    x = (df_canton_year[axis] - df_canton_all.min()) / (df_canton_all.max() - df_canton_all.min())
+    return x
 
 
 if __name__ == '__main__':
