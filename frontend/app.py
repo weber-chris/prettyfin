e1f51b96fddb786cca043783d635f357b1ef7e88
Christoph Weber <christoph.t.weber@gmail.com>
2020-02-06 16:50:27 +0100

    display swiss cantons as map in jupyter notebook

--- frontend/app.py
+++ frontend/app.py
@@ -113,17 +113,17 @@ def render_content(tab):
         ])
     elif tab == 'tab-map':
         return html.Div([
-            html.H3('Tab content 2'),
-            dcc.Graph(
-                id='graph-2-tabs',
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [5, 10, 6],
-                        'type': 'bar'
-                    }]
-                }
-            )
+            html.H3('Swiss Cantons'),
+            # dcc.Graph(
+            #     id='graph-2-tabs',
+            #     figure={
+            #         'data': [{
+            #             'x': [1, 2, 3],
+            #             'y': [5, 10, 6],
+            #             'type': 'bar'
+            #         }]
+            #     }
+            # )
         ])
 
 @app.callback(Output('start-button-text', 'children'), [Input('interval', 'disabled')])
